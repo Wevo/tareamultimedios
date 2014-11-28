f7006cee326143406db1b05656c79fd93dcfade3
Francisco Cabezas <francisco.cabezas@accedo.tv>
2014-11-28 19:25:42 -0300

    - JSON Parse Fix

--- src/cl/telematica/tareamultimedios/MainActivity.java
+++ src/cl/telematica/tareamultimedios/MainActivity.java
@@ -10,17 +10,16 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+public class MainActivity extends Activity implements DownloadListener {
 
-
-public class MainActivity extends Activity {
-
-	  TextView titulors;
+	TextView titulors;
 	  TextView puntors;
 	  TextView linksors;
 	  ImageView imageners;
@@ -29,75 +28,94 @@ public class MainActivity extends Activity {
 	  //JSON Node Names
 	  private static final String TAG_OBJETO ="in";
 	  private static final String TAG_TITULO = "title";
-	  private static final String TAG_PUNTO = "point";
+	  private static final String TAG_PUNTO = "points";
 	  private static final String TAG_LINK = "link";
 	  private static final String TAG_IMAGEN = "image";
 	  Button Btngetdata;
 	  JSONArray post = null;
+	  
+	  private ProgressDialog pDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		titulors = (TextView)findViewById(R.id.titulo);
+      puntors = (TextView)findViewById(R.id.puntos);
+      imageners = (ImageView)findViewById(R.id.imagen); 
+      
+      pDialog = new ProgressDialog(MainActivity.this);
+      pDialog.setMessage("Getting Data ...");
+      pDialog.setIndeterminate(false);
+      pDialog.setCancelable(true);
+		
 		Btngetdata = (Button)findViewById(R.id.getdata);
-        Btngetdata.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-             new JSONParse().execute();
-      }
-    });
+      Btngetdata.setOnClickListener(new View.OnClickListener() {
+	      @Override
+	      public void onClick(View view) {
+	             new DownloadManager(MainActivity.this, 2000, 7000, "GET").execute(url);
+	      }
+	    });
 	}
 
-	private class JSONParse extends AsyncTask<String, String, JSONObject> {
-	       private ProgressDialog pDialog;
-	      @Override
-	        protected void onPreExecute() {
-	            super.onPreExecute();
-	            
-//		         Toast.makeText(getApplicationContext()," Guardado!", Toast.LENGTH_SHORT).show();
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-	             titulors = (TextView)findViewById(R.id.titulo);
-	             puntors = (TextView)findViewById(R.id.puntos);
-	             imageners = (ImageView)findViewById(R.id.imagen); 
-	             
-	            pDialog = new ProgressDialog(MainActivity.this);
-	            pDialog.setMessage("Getting Data ...");
-	            pDialog.setIndeterminate(false);
-	            pDialog.setCancelable(true);
-	            pDialog.show();
-	      }
-	      @Override
-	        protected JSONObject doInBackground(String... args) {
-	        JSONParser jParser = new JSONParser();
-	        // Getting JSON from URL
-	        JSONObject json = jParser.getJSONFromUrl(url);
-	        return json;
-	      }
-	       @Override
-	         protected void onPostExecute(JSONObject json) {
-	         pDialog.dismiss();
-	         //Toast.makeText(getApplicationContext()," Guardado!", Toast.LENGTH_SHORT).show();
-	        
-	         try {
-	            // Getting JSON Array
-	            post = json.getJSONArray(TAG_OBJETO);
-	            JSONObject c = post.getJSONObject(0);
-	            // Storing  JSON item in a Variable
-	            String titulo = c.getString(TAG_TITULO);
-	            String punto = c.getString(TAG_PUNTO);
-	            String link = c.getString(TAG_LINK);
-	            String imagen = c.getString(TAG_IMAGEN); //quiza no??
-	            
-	            //Set JSON Data in TextView
-	            titulors.setText(titulo);
-	            puntors.setText(punto);
-	           // linksors.setText(link);
-	            //imageners.setIma??
-	        } catch (JSONException e) {
-	          e.printStackTrace();
-	        }
-	       }
-	    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
+	@Override
+	public void onRequestStart() {
+		// TODO Auto-generated method stub
+		if(pDialog != null && !pDialog.isShowing()){
+			pDialog.show();
+		}
+	}
 
+	@Override
+	public void onRequestComplete(String data) {
+		// TODO Auto-generated method stub
+		if(pDialog != null && pDialog.isShowing()){
+			pDialog.dismiss();
+		}
+		try {
+		  JSONArray json = new JSONArray(data);
+          // Getting JSON Array
+          JSONObject c = json.getJSONObject(0);
+          // Storing  JSON item in a Variable
+          String titulo = c.getString(TAG_TITULO);
+          String punto = c.getString(TAG_PUNTO);
+          String link = c.getString(TAG_LINK);
+          String imagen = c.getString(TAG_IMAGEN); //quiza no??
+          
+          //Set JSON Data in TextView
+          titulors.setText(titulo);
+          puntors.setText(punto);
+         // linksors.setText(link);
+          //imageners.setIma??
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+	}
 
+	@Override
+	public void onError(String error, int code) {
+		// TODO Auto-generated method stub
+		
+	}  
+	
 }
